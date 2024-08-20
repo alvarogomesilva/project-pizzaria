ba0433f48bd0f27acb7b83787e138717d0b2bdfa
Alvaro Henrique <alvaroindaial15@gmail.com>
2024-08-20 20:33:15 -0300

    feat(auth): gera token jwt

--- backend/src/services/users/AuthUserService.ts
+++ backend/src/services/users/AuthUserService.ts
@@ -1,19 +1,24 @@
 import { compareSync } from "bcryptjs";
 import { Prisma } from "../../config/prisma";
 import { AuthRequest } from "../../types/User";
+import { sign } from "jsonwebtoken";
 
 export const AuthUserService = async ({email, password}: AuthRequest) => {
 
-    const userExists = await Prisma.user.findFirst({
+    const user = await Prisma.user.findFirst({
         where: { email: email }
     })
 
-    if (!userExists) return { messageError: 'Email/password incorrect!' }
+    if (!user) return { messageError: 'Email/password incorrect!' }
 
-    const passwordMatch = compareSync(password, userExists.password)
+    const passwordMatch = compareSync(password, user.password)
 
     if (!passwordMatch) return { messageError: 'Email/password incorrect!' }
 
+    const token = sign({ name: user.name, email: user.email }, process.env.JWT_SECRET, {
+        subject: user.id,
+        expiresIn: '30d'
+    })
 
-    return { ok: true }
+    return { id: user.id, name: user.name, email: user.email, token: token }
 }
